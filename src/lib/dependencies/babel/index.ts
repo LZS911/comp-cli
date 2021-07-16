4f589e87353eadc1c1f9231be53aa447d8eb0fda
lizhensheng <lizhensheng@actionsky.com>
2021-07-16 16:01:42 +0800

    update

--- src/lib/dependencies/babel/index.ts
+++ src/lib/dependencies/babel/index.ts
@@ -1,6 +1,7 @@
 export default function createBabel(name: string, pkg, files) {
   const dependence = {
     devDependencies: {
+      '@babel/core': '^7.14.6',
       '@babel/preset-env': '^7.14.7',
       '@babel/preset-react': '^7.14.5',
       '@babel/preset-typescript': '^7.14.5',

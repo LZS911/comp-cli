7beecae08ed532452817fad5773228f8442b55e8
lizhensheng <lizhensheng@actionsky.com>
2021-08-17 18:46:05 +0800

    update

--- src/lib/dependencies/root/index.ts
+++ src/lib/dependencies/root/index.ts
@@ -53,7 +53,7 @@ export default async function createRoot(
       await executeCommand(`rm -rf ${targetDir}`, cwd);
     }
   }
-  ['prettier', 'indexHtml', 'readme', 'git'].forEach(item => {
+  ['prettier', 'indexHtml', 'readme', 'git', 'git_sh'].forEach(item => {
     const templateInfo = require(`./template/${item}`).default(projectName);
     files[templateInfo.fileName] = templateInfo.template;
   });
---
+++ src/lib/dependencies/root/template/git_sh.ts
@@ -0,0 +1,32 @@
+const generateGit = name => {
+  return {
+    targetDir: `./${name}`,
+    fileName: 'git.sh',
+    template: `# if [ $1 == 'update' ]
+# then
+  git add .
+  git commit -m $1
+  git push
+  echo '代码提交成功'
+# elif [ $1 == 'pull' ]
+# then
+#   git pull
+#   echo '代码拉取成功'
+# elif [ $1 == 'stash' ]
+# then
+#   git stash save $2
+#   echo '提交暂存成功'
+# elif [ $1 == 'pop' ]
+# then
+#   git stash pop $2
+#   echo '提交弹出成功'
+# elif [ $1 == 'apply' ]
+# then
+#   git stash apply $2
+#   echo '提交恢复成功'
+# fi
+  `,
+  };
+};
+
+export default generateGit;
